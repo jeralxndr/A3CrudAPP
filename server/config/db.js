1ba53d9c1656df2694f58e8030fac555f800c95b
jeralxndr <jeremy.alexander4@ontariotechu.net>
2024-11-24 20:21:51 -0500

    mongodb connected

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,3 @@
-module.exports={"URI":"mongodb+srv://umadakram:SX7xvorYdF0anzaV@cluster0.gxyz5.mongodb.net/Finance"}
+module.exports={"URI":"mongodb+srv://jeralxndr1:pokemon*2@cluster0.ctxxd.mongodb.net/ModelsPage"}
 
 
